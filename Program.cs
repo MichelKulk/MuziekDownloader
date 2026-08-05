327e40eabd3f2ba7e7a79b31008577a671de4481
MichelKulk <126677435+MichelKulk@users.noreply.github.com>
2026-08-05 10:56:21 +0200

    Voeg Program.cs toe

---
+++ Program.cs
@@ -0,0 +1,11 @@
+namespace MuziekDownloader;
+
+internal static class Program
+{
+    [STAThread]
+    private static void Main()
+    {
+        ApplicationConfiguration.Initialize();
+        Application.Run(new MainForm());
+    }
+}
